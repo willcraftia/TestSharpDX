b085e72c2e66ce5cec17459e9351b81ae828f586
Will Craftia <willcraftia@gmail.com>
2013-03-04 13:32:07 +0900

    XNA Primitives3D 対応サンプルの実装を開始。

---
+++ Libra/Libra.Samples.Primitives3D/GeometricPrimitive.cs
@@ -0,0 +1,143 @@
+﻿#region Using
+
+using System;
+using System.Collections.Generic;
+using Libra.Graphics;
+
+#endregion
+
+namespace Libra.Samples.Primitives3D
+{
+    public abstract class GeometricPrimitive : IDisposable
+    {
+        List<InputPositionNormal> vertices = new List<InputPositionNormal>();
+
+        List<ushort> indices = new List<ushort>();
+        
+        VertexBuffer vertexBuffer;
+        
+        IndexBuffer indexBuffer;
+        
+        //BasicEffect basicEffect;
+
+        protected IDevice Device { get; private set; }
+
+        protected GeometricPrimitive(IDevice device)
+        {
+            if (device == null) throw new ArgumentNullException("device");
+
+            Device = device;
+        }
+
+        protected void AddVertex(Vector3 position, Vector3 normal)
+        {
+            vertices.Add(new InputPositionNormal(position, normal));
+        }
+
+        protected void AddIndex(int index)
+        {
+            if (index > ushort.MaxValue)
+                throw new ArgumentOutOfRangeException("index");
+
+            indices.Add((ushort) index);
+        }
+
+        protected int CurrentVertex
+        {
+            get { return vertices.Count; }
+        }
+
+        protected void InitializePrimitive()
+        {
+            vertexBuffer = Device.CreateVertexBuffer();
+            vertexBuffer.Usage = ResourceUsage.Immutable;
+            vertexBuffer.Initialize(vertices.ToArray());
+
+            indexBuffer = Device.CreateIndexBuffer();
+            indexBuffer.Format = IndexFormat.SixteenBits;
+            indexBuffer.Usage = ResourceUsage.Immutable;
+            indexBuffer.Initialize(indices.ToArray());
+
+            // プリミティブのレンダリングに使用される BasicEffect を作成します。
+            //basicEffect = new BasicEffect(graphicsDevice);
+            //basicEffect.EnableDefaultLighting();
+        }
+
+        public void Draw(DeviceContext context/*, Effect effect*/)
+        {
+            context.InputAssemblerStage.SetVertexBuffer<InputPositionNormal>(0, vertexBuffer);
+            context.InputAssemblerStage.IndexBuffer = indexBuffer;
+
+            //foreach (EffectPass effectPass in effect.CurrentTechnique.Passes)
+            //{
+            //    effectPass.Apply();
+
+            //    int primitiveCount = indices.Count / 3;
+
+            //    graphicsDevice.DrawIndexedPrimitives(PrimitiveType.TriangleList, 0, 0,
+            //                                         vertices.Count, 0, primitiveCount);
+
+            //}
+        }
+
+        public void Draw(DeviceContext context, Matrix world, Matrix view, Matrix projection, Color color)
+        {
+            // BasicEffect のパラメーターを設定します。
+            //basicEffect.World = world;
+            //basicEffect.View = view;
+            //basicEffect.Projection = projection;
+            //basicEffect.DiffuseColor = color.ToVector3();
+            //basicEffect.Alpha = color.A / 255.0f;
+
+            context.OutputMergerStage.DepthStencilState = DepthStencilState.Default;
+
+            if (color.A < 255)
+            {
+                context.OutputMergerStage.BlendState = BlendState.AlphaBlend;
+            }
+            else
+            {
+                context.OutputMergerStage.BlendState = BlendState.Opaque;
+            }
+
+            // BasicEffect を使用して、モデルを描画します。
+            //Draw(basicEffect);
+        }
+
+        #region IDisposable
+
+        bool disposed;
+
+        ~GeometricPrimitive()
+        {
+            Dispose(false);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposed) return;
+
+            if (disposing)
+            {
+                if (vertexBuffer != null)
+                    vertexBuffer.Dispose();
+
+                if (indexBuffer != null)
+                    indexBuffer.Dispose();
+
+                //if (basicEffect != null)
+                //    basicEffect.Dispose();
+            }
+
+            disposed = true;
+        }
+
+        #endregion
+    }
+}
---
+++ Libra/Libra.Samples.Primitives3D/MainGame.cs
@@ -0,0 +1,177 @@
+﻿#region Using
+
+using System;
+using Libra.Games;
+using Libra.Games.SharpDX;
+using Libra.Graphics;
+using Libra.Compiler;
+
+using SDXWRenderForm = SharpDX.Windows.RenderForm;
+
+#endregion
+
+namespace Libra.Samples.Primitives3D
+{
+    public sealed class MainGame : Game
+    {
+        IGamePlatform platform;
+
+        GraphicsManager graphicsManager;
+
+        VertexShader vertexShader;
+
+        PixelShader pixelShader;
+
+        InputLayout inputLayout;
+
+        VertexBuffer vertexBuffer;
+
+        ConstantBuffer constantBuffer;
+
+        public MainGame()
+        {
+            platform = new SdxFormGamePlatform(this, new SDXWRenderForm());
+            graphicsManager = new GraphicsManager(this);
+        }
+
+        protected override void Initialize()
+        {
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            // ここではテストのために行優先でコンパイル。
+            var compiler = new ShaderCompiler();
+            compiler.RootPath = "Shader";
+            compiler.PackMatrixRowMajor = true;
+
+            var vsBytecode = compiler.CompileFromFile("MiniCube.fx", "VS", VertexShaderProfile.vs_4_0);
+            var psBytecode = compiler.CompileFromFile("MiniCube.fx", "PS", PixelShaderProfile.ps_4_0);
+
+            vertexShader = Device.CreateVertexShader();
+            vertexShader.Initialize(vsBytecode);
+
+            pixelShader = Device.CreatePixelShader();
+            pixelShader.Initialize(psBytecode);
+            
+            inputLayout = Device.CreateInputLayout();
+            inputLayout.Initialize<InputPositionColor>(vsBytecode);
+
+            // メモ
+            //
+            // 左手系 (DirectX 標準) と右手系 (ここでは XNA に倣って右手) の差異により、
+            // SharpDX とは頂点座標 (すなわち法線であり外積の方向) が異なる。
+            // なお、面カリングは頂点から定まる法線を基準とするのみであることから、
+            // 法線方向さえ正しければ左手右手での差異は無い。
+
+            vertexBuffer = Device.CreateVertexBuffer();
+            vertexBuffer.Usage = ResourceUsage.Immutable;
+            vertexBuffer.Initialize(new []
+                {
+                    new InputPositionColor { Position = new Vector3(-1, -1,  1), Color = new Color(255, 0, 0, 255) },
+                    new InputPositionColor { Position = new Vector3(-1,  1,  1), Color = new Color(255, 0, 0, 255) },
+                    new InputPositionColor { Position = new Vector3( 1,  1,  1), Color = new Color(255, 0, 0, 255) },
+                    new InputPositionColor { Position = new Vector3(-1, -1,  1), Color = new Color(255, 0, 0, 255) },
+                    new InputPositionColor { Position = new Vector3( 1,  1,  1), Color = new Color(255, 0, 0, 255) },
+                    new InputPositionColor { Position = new Vector3( 1, -1,  1), Color = new Color(255, 0, 0, 255) },
+
+                    new InputPositionColor { Position = new Vector3(-1, -1, -1), Color = new Color(0, 255, 0, 255) },
+                    new InputPositionColor { Position = new Vector3( 1,  1, -1), Color = new Color(0, 255, 0, 255) },
+                    new InputPositionColor { Position = new Vector3(-1,  1, -1), Color = new Color(0, 255, 0, 255) },
+                    new InputPositionColor { Position = new Vector3(-1, -1, -1), Color = new Color(0, 255, 0, 255) },
+                    new InputPositionColor { Position = new Vector3( 1, -1, -1), Color = new Color(0, 255, 0, 255) },
+                    new InputPositionColor { Position = new Vector3( 1,  1, -1), Color = new Color(0, 255, 0, 255) },
+
+                    new InputPositionColor { Position = new Vector3(-1,  1, -1), Color = new Color(0, 0, 255, 255) },
+                    new InputPositionColor { Position = new Vector3( 1,  1, -1), Color = new Color(0, 0, 255, 255) },
+                    new InputPositionColor { Position = new Vector3( 1,  1,  1), Color = new Color(0, 0, 255, 255) },
+                    new InputPositionColor { Position = new Vector3(-1,  1, -1), Color = new Color(0, 0, 255, 255) },
+                    new InputPositionColor { Position = new Vector3( 1,  1,  1), Color = new Color(0, 0, 255, 255) },
+                    new InputPositionColor { Position = new Vector3(-1,  1,  1), Color = new Color(0, 0, 255, 255) },
+
+                    new InputPositionColor { Position = new Vector3(-1, -1, -1), Color = new Color(255, 255, 0, 255) },
+                    new InputPositionColor { Position = new Vector3(-1, -1,  1), Color = new Color(255, 255, 0, 255) },
+                    new InputPositionColor { Position = new Vector3( 1, -1,  1), Color = new Color(255, 255, 0, 255) },
+                    new InputPositionColor { Position = new Vector3(-1, -1, -1), Color = new Color(255, 255, 0, 255) },
+                    new InputPositionColor { Position = new Vector3( 1, -1,  1), Color = new Color(255, 255, 0, 255) },
+                    new InputPositionColor { Position = new Vector3( 1, -1, -1), Color = new Color(255, 255, 0, 255) },
+
+                    new InputPositionColor { Position = new Vector3(-1, -1, -1), Color = new Color(255, 0, 255, 255) },
+                    new InputPositionColor { Position = new Vector3(-1,  1, -1), Color = new Color(255, 0, 255, 255) },
+                    new InputPositionColor { Position = new Vector3(-1,  1,  1), Color = new Color(255, 0, 255, 255) },
+                    new InputPositionColor { Position = new Vector3(-1, -1, -1), Color = new Color(255, 0, 255, 255) },
+                    new InputPositionColor { Position = new Vector3(-1,  1,  1), Color = new Color(255, 0, 255, 255) },
+                    new InputPositionColor { Position = new Vector3(-1, -1,  1), Color = new Color(255, 0, 255, 255) },
+
+                    new InputPositionColor { Position = new Vector3( 1, -1, -1), Color = new Color(0, 255, 255, 255) },
+                    new InputPositionColor { Position = new Vector3( 1, -1,  1), Color = new Color(0, 255, 255, 255) },
+                    new InputPositionColor { Position = new Vector3( 1,  1,  1), Color = new Color(0, 255, 255, 255) },
+                    new InputPositionColor { Position = new Vector3( 1, -1, -1), Color = new Color(0, 255, 255, 255) },
+                    new InputPositionColor { Position = new Vector3( 1,  1,  1), Color = new Color(0, 255, 255, 255) },
+                    new InputPositionColor { Position = new Vector3( 1,  1, -1), Color = new Color(0, 255, 255, 255) },
+                });
+
+            constantBuffer = Device.CreateConstantBuffer();
+            constantBuffer.Initialize<Matrix>();
+
+            base.LoadContent();
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            var context = Device.ImmediateContext;
+
+            var backBufferWidth = graphicsManager.SwapChain.BackBufferWidth;
+            var backBufferHeight = graphicsManager.SwapChain.BackBufferHeight;
+            var viewport = new Viewport(0, 0, backBufferWidth, backBufferHeight);
+
+            context.InputAssemblerStage.InputLayout = inputLayout;
+            context.InputAssemblerStage.PrimitiveTopology = PrimitiveTopology.TriangleList;
+            context.InputAssemblerStage.SetVertexBuffer<InputPositionColor>(0, vertexBuffer);
+
+            context.VertexShaderStage.VertexShader = vertexShader;
+            context.VertexShaderStage.SetConstantBuffer(0, constantBuffer);
+
+            context.RasterizerStage.Viewport = viewport;
+
+            context.PixelShaderStage.PixelShader = pixelShader;
+
+            context.Clear(ClearOptions.Target | ClearOptions.Depth, Color.CornflowerBlue);
+
+            float time = (float) gameTime.TotalGameTime.TotalSeconds;
+            var world = Matrix.CreateRotationX(time) * Matrix.CreateRotationY(time * 2) * Matrix.CreateRotationZ(time * .7f);
+            var view = Matrix.CreateLookAt(new Vector3(0, 0, -5), new Vector3(0, 0, 0), Vector3.UnitY);
+            var projection = Matrix.CreatePerspectiveFieldOfView(MathHelper.PiOver4, viewport.AspectRatio, 0.1f, 100.0f);
+
+            // メモ
+            //
+            // HLSL デフォルトは列優先 (column_major)。
+            // http://msdn.microsoft.com/en-us/library/bb509706(v=vs.85).aspx
+            //
+            // Matrix 構造体は、SharpDX も XNA も自作 Matrix も、
+            // フィールドの並びが行優先 (HLSL では row_major)。
+            //
+            // このため、HLSL デフォルトでは列優先に対応させる必要があり、
+            // このためには行列を転置させてから設定すれば良い。
+            //
+            // あるいは、シェーダをコンパイルする際に、オプション指定により
+            // 行優先と列優先を設定できる。
+            //
+            // なお、列優先の方が効率的であるらしい。
+            // http://maverickproj.web.fc2.com/d3d11_02.html
+            //
+            // 基本的には、デフォルトに従う事を重視し、
+            // 転置してから設定という手順を踏むが、
+            // ここではテストのためにコンパイル時に行優先としている。
+
+            var worldViewProjection = world * view * projection;
+            //worldViewProjection.Transpose();
+            constantBuffer.SetData(context, worldViewProjection);
+
+            context.Draw(36);
+
+            base.Draw(gameTime);
+        }
+    }
+}
