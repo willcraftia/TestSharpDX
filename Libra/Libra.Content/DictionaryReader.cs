4fca863dafc91ef4cd4f80bc8099644935a5f47a
Will Craftia <willcraftia@gmail.com>
2013-03-16 15:31:25 +0900

    IList と IDictionary のためのジェネリック ContentTypeReader。

---
+++ Libra/Libra.Content/DictionaryReader.cs
@@ -0,0 +1,47 @@
+﻿#region Using
+
+using System;
+using System.Collections.Generic;
+
+#endregion
+
+namespace Libra.Content
+{
+    public sealed class DictionaryReader<K, V> : ContentTypeReader<Dictionary<K, V>>
+    {
+        ContentTypeReader keyReader;
+
+        ContentTypeReader valueReader;
+
+        protected internal override void Initialize(ContentTypeReaderManager manager)
+        {
+            keyReader = manager[typeof(K)];
+            valueReader = manager[typeof(V)];
+
+            base.Initialize(manager);
+        }
+
+        protected internal override Dictionary<K, V> Read(ContentReader input, Dictionary<K, V> existingInstance)
+        {
+            var result = existingInstance ?? new Dictionary<K, V>();
+            result.Clear();
+
+            // TODO
+            //
+            // クラス型での null および多態性に関する考慮が必要。
+            // 当面、null 禁止および多態性禁止として進める。
+
+            var count = (int) input.ReadUInt32();
+
+            for (int i = 0; i < count; i++)
+            {
+                var key = input.ReadObject<K>(keyReader);
+                var value = input.ReadObject<V>(valueReader);
+                
+                result[key] = value;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Libra/Libra.Content/ListReader.cs
@@ -0,0 +1,41 @@
+﻿#region Using
+
+using System;
+using System.Collections.Generic;
+
+#endregion
+
+namespace Libra.Content
+{
+    [ContentTypeReader]
+    public sealed class ListReader<T> : ContentTypeReader<IList<T>>
+    {
+        ContentTypeReader itemReader;
+
+        protected internal override void Initialize(ContentTypeReaderManager manager)
+        {
+            itemReader = manager[typeof(T)];
+
+            base.Initialize(manager);
+        }
+
+        protected internal override IList<T> Read(ContentReader input, IList<T> existingInstance)
+        {
+            var result = existingInstance ?? new List<T>();
+            result.Clear();
+
+            // TODO
+            //
+            // クラス型での null および多態性に関する考慮が必要。
+            // 当面、null 禁止および多態性禁止として進める。
+
+            var count = (int) input.ReadUInt32();
+            for (int i = 0; i < count; i++)
+            {
+                result.Add(input.ReadObject<T>(itemReader));
+            }
+
+            return result;
+        }
+    }
+}
