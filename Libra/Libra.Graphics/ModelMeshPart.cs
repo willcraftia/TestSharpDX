e7c7251972f218f16d20ef086b284aa545187b5a
Will Craftia <willcraftia@gmail.com>
2013-03-20 00:27:44 +0900

    プロパティ追加。

--- Libra/Libra.Graphics/ModelMeshPart.cs
+++ Libra/Libra.Graphics/ModelMeshPart.cs
@@ -8,5 +8,17 @@ namespace Libra.Graphics
 {
     public sealed class ModelMeshPart
     {
+        public VertexBuffer VertexBuffer { get; private set; }
+
+        public IndexBuffer IndexBuffer { get; private set; }
+
+        public ModelMeshPart(VertexBuffer vertexBuffer, IndexBuffer indexBuffer)
+        {
+            if (vertexBuffer == null) throw new ArgumentNullException("vertexBuffer");
+            if (indexBuffer == null) throw new ArgumentNullException("indexBuffer");
+
+            VertexBuffer = vertexBuffer;
+            IndexBuffer = indexBuffer;
+        }
     }
 }
