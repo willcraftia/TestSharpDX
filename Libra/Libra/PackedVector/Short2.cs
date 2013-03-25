54fdb6137a1682c07868ce27ac6d80016a24ab1c
Will Craftia <willcraftia@gmail.com>
2013-03-26 01:45:22 +0900

    XNA と同じ書式に変更。

--- Libra/Libra/PackedVector/Short2.cs
+++ Libra/Libra/PackedVector/Short2.cs
@@ -92,7 +92,7 @@ namespace Libra.PackedVector
 
         public override string ToString()
         {
-            return "{packedValue:" + packedValue + "}";
+            return "{" + packedValue.ToString("X8") + "}";
         }
 
         #endregion
