3ca390c1d19c66fb13b86d30579ca3fd1badd843
Will Craftia <willcraftia@gmail.com>
2013-03-26 15:39:08 +0900

    オクルージョン クエリの確認のために XNA LensFlare サンプルを移植。

---
+++ Libra/Libra.Samples.LensFlare/LensFlareComponent.cs
@@ -0,0 +1,245 @@
+﻿#region Using
+
+using System;
+using Libra.Games;
+using Libra.Graphics;
+
+#endregion
+
+namespace Libra.Samples.LensFlare
+{
+    public sealed class LensFlareComponent : DrawableGameComponent
+    {
+        const float glowSize = 400;
+
+        const float querySize = 100;
+
+        public Matrix View;
+
+        public Matrix Projection;
+
+        public Vector3 LightDirection = Vector3.Normalize(new Vector3(-1, -0.1f, 0.3f));
+
+        Vector2 lightPosition;
+        
+        bool lightBehindCamera;
+
+        Texture2D glowSprite;
+
+        ShaderResourceView glowSpriteView;
+
+        SpriteBatch spriteBatch;
+        
+        BasicEffect basicEffect;
+        
+        VertexBuffer vertexBuffer;
+
+        static readonly BlendState ColorWriteDisable = new BlendState
+        {
+            ColorWriteChannels = ColorWriteChannels.None
+        };
+
+        OcclusionQuery occlusionQuery;
+
+        bool occlusionQueryActive;
+        
+        float occlusionAlpha;
+
+        #region Flare
+
+        class Flare
+        {
+            public Flare(float position, float scale, Color color, string textureName)
+            {
+                Position = position;
+                Scale = scale;
+                Color = color;
+                TextureName = textureName;
+            }
+
+            public float Position;
+
+            public float Scale;
+            
+            public Color Color;
+            
+            public string TextureName;
+
+            public Texture2D Texture;
+
+            public ShaderResourceView TextureView;
+        }
+
+        #endregion
+
+        Flare[] flares =
+        {
+            new Flare(-0.5f, 0.7f, new Color( 50,  25,  50), "flare1"),
+            new Flare( 0.3f, 0.4f, new Color(100, 255, 200), "flare1"),
+            new Flare( 1.2f, 1.0f, new Color(100,  50,  50), "flare1"),
+            new Flare( 1.5f, 1.5f, new Color( 50, 100,  50), "flare1"),
+
+            new Flare(-0.3f, 0.7f, new Color(200,  50,  50), "flare2"),
+            new Flare( 0.6f, 0.9f, new Color( 50, 100,  50), "flare2"),
+            new Flare( 0.7f, 0.4f, new Color( 50, 200, 200), "flare2"),
+
+            new Flare(-0.7f, 0.7f, new Color( 50, 100,  25), "flare3"),
+            new Flare( 0.0f, 0.6f, new Color( 25,  25,  25), "flare3"),
+            new Flare( 2.0f, 1.4f, new Color( 25,  50, 100), "flare3"),
+        };
+
+        public LensFlareComponent(Game game)
+            : base(game)
+        {
+        }
+
+        protected override void LoadContent()
+        {
+            spriteBatch = new SpriteBatch(Device.ImmediateContext);
+
+            glowSprite = (Game as MainGame).Content.Load<Texture2D>("glow");
+            glowSpriteView = Device.CreateShaderResourceView();
+            glowSpriteView.Initialize(glowSprite);
+
+            foreach (var flare in flares)
+            {
+                flare.Texture = (Game as MainGame).Content.Load<Texture2D>(flare.TextureName);
+                flare.TextureView = Device.CreateShaderResourceView();
+                flare.TextureView.Initialize(flare.Texture);
+            }
+
+            basicEffect = new BasicEffect(Device);
+            basicEffect.View = Matrix.Identity;
+            basicEffect.VertexColorEnabled = true;
+
+            var vertices = new VertexPositionColor[4];
+            vertices[0].Position = new Vector3(-querySize / 2, -querySize / 2, -1);
+            vertices[1].Position = new Vector3( querySize / 2, -querySize / 2, -1);
+            vertices[2].Position = new Vector3(-querySize / 2,  querySize / 2, -1);
+            vertices[3].Position = new Vector3( querySize / 2,  querySize / 2, -1);
+
+            vertexBuffer = Device.CreateVertexBuffer();
+            vertexBuffer.Usage = ResourceUsage.Immutable;
+            vertexBuffer.Initialize(vertices);
+
+            occlusionQuery = Device.CreateOcclusionQuery();
+            occlusionQuery.Initialize();
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+            UpdateOcclusion();
+
+            DrawGlow();
+            DrawFlares();
+
+            RestoreRenderStates();
+        }
+
+        public void UpdateOcclusion()
+        {
+            var context = Device.ImmediateContext;
+
+            var infiniteView = View;
+
+            infiniteView.Translation = Vector3.Zero;
+
+            var viewport = context.Viewport;
+
+            var projectedPosition = viewport.Project(-LightDirection, Projection, infiniteView, Matrix.Identity);
+
+            if ((projectedPosition.Z < 0) || (projectedPosition.Z > 1))
+            {
+                lightBehindCamera = true;
+                return;
+            }
+
+            lightPosition = new Vector2(projectedPosition.X, projectedPosition.Y);
+            lightBehindCamera = false;
+
+            if (occlusionQueryActive)
+            {
+                if (!occlusionQuery.IsComplete)
+                    return;
+
+                const float queryArea = querySize * querySize;
+
+                occlusionAlpha = Math.Min(occlusionQuery.PixelCount / queryArea, 1);
+            }
+
+            context.BlendState = ColorWriteDisable;
+            context.DepthStencilState = DepthStencilState.DepthRead;
+
+            context.PrimitiveTopology = PrimitiveTopology.TriangleStrip;
+            context.SetVertexBuffer(0, vertexBuffer);
+
+            basicEffect.World = Matrix.CreateTranslation(lightPosition.X, lightPosition.Y, 0);
+
+            basicEffect.Projection = Matrix.CreateOrthographicOffCenter(
+                0, viewport.Width, viewport.Height, 0, 0, 1);
+
+            basicEffect.Apply(context);
+
+            occlusionQuery.Begin();
+
+            context.Draw(4);
+
+            occlusionQuery.End();
+
+            occlusionQueryActive = true;
+        }
+
+        public void DrawGlow()
+        {
+            if (lightBehindCamera || occlusionAlpha <= 0)
+                return;
+
+            var color = Color.White * occlusionAlpha;
+            var origin = new Vector2(glowSprite.Width, glowSprite.Height) / 2;
+            float scale = glowSize * 2 / glowSprite.Width;
+
+            spriteBatch.Begin();
+            spriteBatch.Draw(glowSpriteView, lightPosition, null, color, 0, origin, scale, SpriteEffects.None, 0);
+            spriteBatch.End();
+        }
+
+        public void DrawFlares()
+        {
+            if (lightBehindCamera || occlusionAlpha <= 0)
+                return;
+
+            var viewport = Device.ImmediateContext.Viewport;
+            var screenCenter = new Vector2(viewport.Width, viewport.Height) / 2;
+
+            var flareVector = screenCenter - lightPosition;
+
+            spriteBatch.Begin(0, BlendState.Additive);
+
+            foreach (var flare in flares)
+            {
+                var flarePosition = lightPosition + flareVector * flare.Position;
+
+                var flareColor = flare.Color.ToVector4();
+
+                flareColor.W *= occlusionAlpha;
+
+                var flareOrigin = new Vector2(flare.Texture.Width, flare.Texture.Height) / 2;
+
+                spriteBatch.Draw(flare.TextureView, flarePosition, null,
+                                 new Color(flareColor), 1, flareOrigin,
+                                 flare.Scale, SpriteEffects.None, 0);
+            }
+
+            spriteBatch.End();
+        }
+
+        void RestoreRenderStates()
+        {
+            var context = Device.ImmediateContext;
+
+            context.BlendState = BlendState.Opaque;
+            context.DepthStencilState = DepthStencilState.Default;
+            context.PixelShaderSamplers[0] = SamplerState.LinearWrap;
+        }
+    }
+}
---
+++ Libra/Libra.Samples.LensFlare/MainGame.cs
@@ -0,0 +1,197 @@
+﻿#region Using
+
+using System;
+using Libra.Games;
+using Libra.Games.SharpDX;
+using Libra.Graphics;
+using Libra.Input;
+using Libra.Xnb;
+
+#endregion
+
+namespace Libra.Samples.LensFlare
+{
+    public sealed class MainGame : Game
+    {
+        IGamePlatform platform;
+
+        GraphicsManager graphics;
+
+        IKeyboard keyboard;
+
+        IJoystick joystick;
+
+        KeyboardState currentKeyboardState = new KeyboardState();
+
+        JoystickState currentJoystickState = new JoystickState();
+        
+        Vector3 cameraPosition = new Vector3(-200, 30, 30);
+        
+        Vector3 cameraFront = new Vector3(1, 0, 0);
+
+        Model terrain;
+
+        LensFlareComponent lensFlare;
+
+        internal XnbManager Content { get; private set; }
+
+        public MainGame()
+        {
+            platform = new SdxFormGamePlatform(this)
+            {
+                DirectInputEnabled = true
+            };
+            graphics = new GraphicsManager(this);
+
+            Content = new XnbManager(Services, "Content");
+
+            lensFlare = new LensFlareComponent(this);
+
+            Components.Add(lensFlare);
+        }
+
+        protected override void LoadContent()
+        {
+            terrain = Content.Load<Model>("terrain");
+
+            keyboard = platform.CreateKeyboard();
+            joystick = platform.CreateJoystick();
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            HandleInput();
+
+            UpdateCamera(gameTime);
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            var context = Device.ImmediateContext;
+
+            context.Clear(Color.CornflowerBlue);
+
+            var view = Matrix.CreateLookAt(cameraPosition, cameraPosition + cameraFront, Vector3.Up);
+
+            var aspectRatio = context.Viewport.AspectRatio;
+            var projection = Matrix.CreatePerspectiveFieldOfView(MathHelper.PiOver4, aspectRatio, 0.1f, 500);
+
+            context.RasterizerState = RasterizerState.CullNone;
+
+            foreach (var mesh in terrain.Meshes)
+            {
+                foreach (BasicEffect effect in mesh.Effects)
+                {
+                    effect.World = Matrix.Identity;
+                    effect.View = view;
+                    effect.Projection = projection;
+
+                    effect.LightingEnabled = true;
+                    effect.DiffuseColor = new Vector3(1f);
+                    effect.AmbientLightColor = new Vector3(0.5f);
+
+                    effect.DirectionalLights[0].Enabled = true;
+                    effect.DirectionalLights[0].DiffuseColor = Vector3.One;
+                    effect.DirectionalLights[0].Direction = lensFlare.LightDirection;
+
+                    effect.FogEnabled = true;
+                    effect.FogStart = 200;
+                    effect.FogEnd = 500;
+                    effect.FogColor = Color.CornflowerBlue.ToVector3();
+                }
+
+                mesh.Draw(context);
+            }
+
+            lensFlare.View = view;
+            lensFlare.Projection = projection;
+
+            base.Draw(gameTime);
+        }
+
+        void HandleInput()
+        {
+            currentKeyboardState = keyboard.GetState();
+            currentJoystickState = joystick.GetState();
+
+            if (currentKeyboardState.IsKeyDown(Keys.Escape) ||
+                currentJoystickState.Buttons.Back == ButtonState.Pressed)
+            {
+                Exit();
+            }
+        }
+
+        void UpdateCamera(GameTime gameTime)
+        {
+            float time = (float)gameTime.ElapsedGameTime.TotalMilliseconds;
+
+            float pitch = -currentJoystickState.ThumbSticks.Right.Y * time * 0.001f;
+            float turn = -currentJoystickState.ThumbSticks.Right.X * time * 0.001f;
+
+            if (currentKeyboardState.IsKeyDown(Keys.Up))
+                pitch += time * 0.001f;
+
+            if (currentKeyboardState.IsKeyDown(Keys.Down))
+                pitch -= time * 0.001f;
+
+            if (currentKeyboardState.IsKeyDown(Keys.Left))
+                turn += time * 0.001f;
+
+            if (currentKeyboardState.IsKeyDown(Keys.Right))
+                turn -= time * 0.001f;
+
+            var cameraRight = Vector3.Cross(Vector3.Up, cameraFront);
+            var flatFront = Vector3.Cross(cameraRight, Vector3.Up);
+
+            var pitchMatrix = Matrix.CreateFromAxisAngle(cameraRight, pitch);
+            var turnMatrix = Matrix.CreateFromAxisAngle(Vector3.Up, turn);
+
+            var tiltedFront = Vector3.TransformNormal(cameraFront, pitchMatrix * turnMatrix);
+
+            if (Vector3.Dot(tiltedFront, flatFront) > 0.001f)
+            {
+                cameraFront = Vector3.Normalize(tiltedFront);
+            }
+
+            if (currentKeyboardState.IsKeyDown(Keys.W))
+                cameraPosition += cameraFront * time * 0.1f;
+            
+            if (currentKeyboardState.IsKeyDown(Keys.S))
+                cameraPosition -= cameraFront * time * 0.1f;
+
+            if (currentKeyboardState.IsKeyDown(Keys.A))
+                cameraPosition += cameraRight * time * 0.1f;
+
+            if (currentKeyboardState.IsKeyDown(Keys.D))
+                cameraPosition -= cameraRight * time * 0.1f;
+
+            cameraPosition += cameraFront * currentJoystickState.ThumbSticks.Left.Y * time * 0.1f;
+            cameraPosition -= cameraRight * currentJoystickState.ThumbSticks.Left.X * time * 0.1f;
+
+            if (currentJoystickState.Buttons.RightStick == ButtonState.Pressed ||
+                currentKeyboardState.IsKeyDown(Keys.R))
+            {
+                cameraPosition = new Vector3(-200, 30, 30);
+                cameraFront = new Vector3(1, 0, 0);
+            }
+        }
+    }
+
+    #region Program
+
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new MainGame())
+            {
+                game.Run();
+            }
+        }
+    }
+
+    #endregion
+}
