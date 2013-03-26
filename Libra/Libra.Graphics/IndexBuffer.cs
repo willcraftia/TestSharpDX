fc9510bdab1414c9b417b37d114b1613fbc8f088
Will Craftia <willcraftia@gmail.com>
2013-03-26 16:09:42 +0900

    検査処理の追加。

--- Libra/Libra.Graphics/IndexBuffer.cs
+++ Libra/Libra.Graphics/IndexBuffer.cs
@@ -9,18 +9,32 @@ namespace Libra.Graphics
 {
     public abstract class IndexBuffer : Resource
     {
-        public IndexFormat Format { get; set; }
+        bool initialized;
+
+        IndexFormat format;
+
+        public IndexFormat Format
+        {
+            get { return format; }
+            set
+            {
+                AssertNotInitialized();
+
+                format = value;
+            }
+        }
 
         public int IndexCount { get; private set; }
 
         protected IndexBuffer(IDevice device)
             : base(device)
         {
-            Format = IndexFormat.SixteenBits;
+            format = IndexFormat.SixteenBits;
         }
 
         public void Initialize(int indexCount)
         {
+            AssertNotInitialized();
             if (indexCount < 1) throw new ArgumentOutOfRangeException("indexCount");
 
             if (Usage == ResourceUsage.Immutable)
@@ -29,10 +43,13 @@ namespace Libra.Graphics
             IndexCount = indexCount;
 
             InitializeCore();
+
+            initialized = true;
         }
 
         public void Initialize<T>(T[] data) where T : struct
         {
+            AssertNotInitialized();
             if (data == null) throw new ArgumentNullException("data");
             if (data.Length == 0) throw new ArgumentException("Data must be not empty.", "data");
 
@@ -42,22 +59,37 @@ namespace Libra.Graphics
             IndexCount = totalSizeInBytes / FormatHelper.SizeInBytes(Format);
 
             InitializeCore(data);
+
+            initialized = true;
         }
 
-        protected abstract void InitializeCore();
+        public void GetData<T>(DeviceContext context, T[] data, int startIndex, int elementCount) where T : struct
+        {
+            AssetInitialized();
+            if (context == null) throw new ArgumentNullException("context");
+            if (data == null) throw new ArgumentNullException("data");
+            if (startIndex < 1) throw new ArgumentOutOfRangeException("startIndex");
+            if (data.Length < (startIndex + elementCount)) throw new ArgumentOutOfRangeException("elementCount");
 
-        protected abstract void InitializeCore<T>(T[] data) where T : struct;
-
-        public abstract void GetData<T>(DeviceContext context, T[] data, int startIndex, int elementCount) where T : struct;
+            GetDataCore(context, data, startIndex, elementCount);
+        }
 
         public void GetData<T>(DeviceContext context, T[] data) where T : struct
         {
-            GetData(context, data, 0, data.Length);
+            AssetInitialized();
+            if (context == null) throw new ArgumentNullException("context");
+            if (data == null) throw new ArgumentNullException("data");
+
+            GetDataCore(context, data, 0, data.Length);
         }
 
         public void SetData<T>(DeviceContext context, T[] data, int startIndex, int elementCount) where T : struct
         {
+            AssetInitialized();
+            if (context == null) throw new ArgumentNullException("context");
             if (data == null) throw new ArgumentNullException("data");
+            if (startIndex < 1) throw new ArgumentOutOfRangeException("startIndex");
+            if (data.Length < (startIndex + elementCount)) throw new ArgumentOutOfRangeException("elementCount");
 
             if (Usage == ResourceUsage.Immutable)
                 throw new InvalidOperationException("Data can not be set from CPU.");
@@ -104,6 +136,22 @@ namespace Libra.Graphics
         public void SetData<T>(DeviceContext context, params T[] data) where T : struct
         {
             SetData(context, data, 0, data.Length);
+        }
+
+        protected abstract void InitializeCore();
+
+        protected abstract void InitializeCore<T>(T[] data) where T : struct;
+
+        protected abstract void GetDataCore<T>(DeviceContext context, T[] data, int startIndex, int elementCount) where T : struct;
+
+        void AssertNotInitialized()
+        {
+            if (initialized) throw new InvalidOperationException("Already initialized.");
+        }
+
+        void AssetInitialized()
+        {
+            if (!initialized) throw new InvalidOperationException("Not initialized.");
         }
     }
 }
