d6641f3bb375491d4d75acd0bea5b1c538739156
Will Craftia <willcraftia@gmail.com>
2013-03-28 19:57:17 +0900

    コメント追記。

--- Libra/Libra.Graphics/VertexBufferBinding.cs
+++ Libra/Libra.Graphics/VertexBufferBinding.cs
@@ -10,6 +10,10 @@ namespace Libra.Graphics
     {
         public VertexBuffer VertexBuffer;
 
+        // バイト単位でのオフセット。
+        // ModelMeshPart の VertexOffset は、
+        // XNA 仕様に合わせて頂点数単位でのオフセットであるため差異に注意。
+        // ModelMeshPart の VertexOffset は、頂点ストライドを掛ける事でバイト単位となる。
         public int Offset;
 
         public VertexBufferBinding(VertexBuffer vertexBuffer, int offset)
